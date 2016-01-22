8295e890f0e384cab4773bd503e67d9ba5e79e66
quentin hernandez <quentin hernandez>
2016-01-22 16:16:28 +0100

    caca 	modified:   Serveur/src/GestionProtocoleServeur.java 	modified:   Serveur/src/MainServeur.java

--- Serveur/src/GestionProtocoleServeur.java
+++ Serveur/src/GestionProtocoleServeur.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
-public class GestionProtocoleServeur {
 
+
+public class GestionProtocoleServeur extends Thread {
+	
+	private boolean fermeture;
+	private Socket connexionCourante;
+	private InputStream entreeSocket;
+	private OutputStream sortieSocket;
+	//private Gestion gestion;
+	
+	String chaine;
+	
+	public GestionProtocoleServeur(Socket connexionCourante, InputStream entreeSocket,
+			OutputStream sortieSocket/*, Gestion gestion*/) {
+
+		//this.gestion = gestion;
+		this.chaine = null;
+		this.connexionCourante = connexionCourante;
+		this.entreeSocket = entreeSocket;
+		this.sortieSocket = sortieSocket;
+		this.fermeture = false;
+	}
 }
