d7e9bf44a3e2e847d9052876ac34b5fc015d768f
quentin hernandez <quentin hernandez>
2016-01-22 16:17:17 +0100

    Serveur 	new file:   Serveur/src/Traitement.java

---
+++ Serveur/src/Traitement.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+
+public class Traitement extends Thread {
+
+	private boolean fermeture;
+	private Socket connexionCourante;
+	private InputStream entreeSocket;
+	private OutputStream sortieSocket;
+	private String requete;
+	private String motClef;
+//	private BDD BDD;
+
+	String chaine;
+
+	public Traitement(Socket connexionCourante, InputStream entreeSocket,
+			OutputStream sortieSocket, String requete) {
+
+//		this.gestion = gestion;
+		this.chaine = null;
+		this.connexionCourante = connexionCourante;
+		this.entreeSocket = entreeSocket;
+		this.sortieSocket = sortieSocket;
+		this.fermeture = false;
+		this.requete = null;
+		
+	}
+
+	public void run() {
+
+		while (!fermeture) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					entreeSocket));
+			
+			try {
+				requete=reader.readLine();
+				
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			String[] tabRequete=requete.split("|");
+			motClef=tabRequete[0];
+			/*****/
+			switch (motClef){
+			case "inscription":
+				BDD.insert(tabRequete[1],tabRequete[2],tabRequete[3],tabRequete[4],tabRequete[5],tabRequete[6], tabRequete[7],tabRequete[8],tabRequete[9] );
+				break;
+			case "connexion":
+				 
+				break;
+			case "consulter":
+				 
+				break;
+			case "modifier":
+				 
+				break;
+			case "recherche":
+				 
+				break;
+			case "déconnexion":
+				 
+				break;
+			default:
+				System.out.println("ERREUR (défaut)");
+			}
+			
+			
+			
+			/****/
+			System.out.println("serveur envoie la chaine= " + chaine);
+			
+			if (chaine != null) {
+				PrintStream pStream = new PrintStream(sortieSocket);
+				pStream.println(chaine);
+			} else
+				fermeture = true;
+		}
+
+		try {
+			System.out.println("fermeture connexion " + connexionCourante);
+			connexionCourante.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
