75a3d46e76b11996b36eff27985f713112d9d0fd
Shuntor <v.iungmann@gmail.com>
2016-01-27 13:46:17 +0100

    27/01

--- Serveur/src/main/java/STRI/JavaConnect/BDD.java
+++ Serveur/src/main/java/STRI/JavaConnect/BDD.java
@@ -1,5 +1,24 @@
 package STRI.JavaConnect;
 
-public class BDD {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BDD {
+	/* La liste qui contiendra tous les résultats de nos essais */
+    private List<String> messages = new ArrayList<String>();
+
+    public List<String> executerTests( String requete ) {
+    	/* Chargement du driver JDBC pour MySQL */
+    	try {
+    	    Class.forName( "com.mysql.jdbc.Driver" );
+	    
+    	} catch ( ClassNotFoundException e ) {
+    	    /* Gérer les éventuelles erreurs ici. */
+    	}
+    	
+    	
+    	
+
+        return messages;
+    } 
 }
