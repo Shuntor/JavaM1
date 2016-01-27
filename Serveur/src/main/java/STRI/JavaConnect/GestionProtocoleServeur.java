7a75e7740641c706ae0409b6ba72984c92988041
Shuntor <v.iungmann@gmail.com>
2016-01-27 14:48:17 +0100

    Gestion protocole serveur

--- Serveur/src/main/java/STRI/JavaConnect/GestionProtocoleServeur.java
+++ Serveur/src/main/java/STRI/JavaConnect/GestionProtocoleServeur.java
@@ -1,6 +1,36 @@
+package STRI.JavaConnect;
+
+
+
 
 public class GestionProtocoleServeur {
 	
+//	public void analyser (String requete){
+//	if(requete.startsWith("connexion")==true){
+//		String[] parts = requete.split("|");
+//		String part0 = parts[0]; 
+//		String part1 = parts[1];
+//		String part2 = parts[2];
+//		
+//		V.maMethode(part1, sommeIni);
+//	}
 	
 	
+	
+	/****
+	 * serialisation
+	 * @param args
+	 * @return
+	 * Mise en forme des trames
+	 */
+	public String serialisation(String... args){
+		String chaine = "";
+		for (String arg:args){
+			chaine+=arg;
+			chaine+="#";
+		}
+		return chaine;
+	}
+
 }
+
