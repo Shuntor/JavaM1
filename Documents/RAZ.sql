9cf937c67a2381f710bfa2a85d88085e455218aa
Shuntor <v.iungmann@gmail.com>
2016-01-31 17:30:02 +0100

    Update RAZ.sql

--- Documents/RAZ.sql
+++ Documents/RAZ.sql
@@ -17,7 +17,7 @@ create table Competences(
 
 create table Acquerir(
 	mail varchar(80),
-	description varchar(300),
+	description varchar(200),
 	constraint pk_acquerir PRIMARY KEY(mail, description),
 	constraint fk_acquerir_utlisateur foreign key(mail) references Utilisateurs(mail),
 	constraint fk_acquerir_competences foreign key(description) references Competences(description)
